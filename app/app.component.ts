02ce8439267f78879d71b5e65b512b401fafd152
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-14 17:01:34 +0530

    surgeignore updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Http } from '@angular/http';
 
 @Component({
 	selector: 'main-component',
-	template: `<h1>AngularAttack 2016</h1><p>Your project runs!-Team</p>`,
+	template: `<h1>AngularAttack 2016</h1><p>Your project runs!-TeamUp</p>`,
 })
 export class AppComponent {
 }
