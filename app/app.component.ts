5ccb1c0d80f41c77a630ab7d0cfb8de3037a9afa
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-14 14:39:12 +0530

    app.component added

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+	selector: 'main-component',
+	template: `<h1>AngularAttack 2016</h1><p>Your project runs!-Team</p>`,
+})
+export class AppComponent {
+}
