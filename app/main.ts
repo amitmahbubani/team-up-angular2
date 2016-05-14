9ae8e2e6bb1861d2952d94bcff41f63b7ee4314e
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-14 18:11:38 +0530

    route config added

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import { Http } from '@angular/http';
 import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+bootstrap(AppComponent, [ROUTER_PROVIDERS]);
