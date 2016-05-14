7366f72cd03bf47f5b03d00e98112a18f3ff9bbf
Gautam Singh <gtm93singh@gmail.com>
2016-05-14 20:51:08 +0530

    Added events endpoint for a particular user

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -5,10 +5,6 @@ var userModel = require(__dirname + '/../models/user');
 
 router.use(function (req, res, next) {
     req.isAuthorized = true;
-    next();
-});
-
-router.get('/profile', function (req, res, next) {
     if (!req.isAuthorized) {
         req.apiResponse = {
             error: {
@@ -16,13 +12,42 @@ router.get('/profile', function (req, res, next) {
                 msg: "Invalid request"
             }
         };
+    }
+    next();
+});
+
+router.all('/profile', function (req, res, next) {
+    if (!req.isAuthorized) {
         return next();
     }
     var params = req.parsedParams;
-    userModel.profile(params.id, function (err, result) {
-        req.apiResponse = result;
+    userModel.profile(params.userId, function (err, result) {
+        if (err) {
+            req.apiResponse = {
+                error: err
+            };
+        } else {
+            req.apiResponse = result;
+        }
         next();
     });
+});
+
+router.all('/events', function (req, res, next) {
+    if (!req.isAuthorized) {
+        return next();
+    }
+    var params = req.parsedParams;
+    userModel.userEvents(params.userId, function (err, result) {
+        if (err) {
+            req.apiResponse = {
+                error: err
+            }
+        } else {
+            req.apiResponse = result;
+        }
+        next();
+    })
 });
 
 module.exports = router;
