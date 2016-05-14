7625b84c4b2f1faa4c7860d21ad2edd73d87e053
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-14 19:53:44 +0530

    base service defined

---
+++ app/components/common/base.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import {Cookie} from './cookie';
+
+@Injectable()
+export class BaseService {
+	private apiUrl = couponDuniaNamespace.config.apiEndpoint;
+	public guestToken = null;
+	public isAuthorized = false;
+	public isLoggedIn = false;
+	constructor(private http: Http) {
+		var authorizeCookie = Cookie.getCookie('isAuthorized');
+		var isAuthorized = (authorizeCookie && authorizeCookie === 'true') ? true : false;
+		this.isLoggedIn = isAuthorized;
+	}
+}
