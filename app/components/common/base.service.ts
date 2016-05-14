72384647149d22e7be4f32809310230aa051e505
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-14 20:10:17 +0530

    guest request added

--- app/components/common/base.service.ts
+++ app/components/common/base.service.ts
@@ -8,6 +8,8 @@ export class BaseService {
 	public guestToken = null;
 	public isAuthorized = false;
 	public isLoggedIn = false;
+	public guestRequested = false;
+	public guestRequestCount = 0;
 	constructor(private http: Http) {
 		var authorizeCookie = Cookie.getCookie('isAuthorized');
 		var isAuthorized = (authorizeCookie && authorizeCookie === 'true') ? true : false;
@@ -18,7 +20,34 @@ export class BaseService {
 		return new Headers({
 			'Authorization': 'Basic ' + btoa(token + ":"),
 			'Content-Type': 'application/json',
-			'X-Request-Source': 'WEB'
 		});
+	}
+
+	getGuestToken(){
+		var serviceUrl = this.apiUrl + '/guest';
+		if(this.guestRequestCount > 5){
+			return;
+		}
+		if (this.guestRequested) {
+			return Rx.Observable.create(function(observer) {
+				setTimeout(res => { observer.next('sdfsd'); }, 2000);
+			})
+		}
+		this.guestRequested = true;
+		this.guestRequestCount++;
+		var a = this.http.get(serviceUrl)
+		.map(res => { return res.json() })
+		.map(res=> {
+			if (res.success === true) {
+					this.guestRequestCount = 0;
+					Cookie.setCookie('auth', res.response.access_token, 1);
+					this.guestRequested = false;
+					return res.response;
+				} else {
+					this.guestRequested = false;
+					return res;
+			}
+		})
+		return a;
 	}
 }
