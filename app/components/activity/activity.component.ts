1389bd64da46932c11176281780c91bd7302376e
Amit Mahbubani <amit.mahbubani@coupondunia.in>
2016-05-15 00:44:20 +0530

    add activity component

---
+++ app/components/activity/activity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+
+@Component({
+	selector: 'activity-component',
+	templateUrl: 'app/components/activity/activity.html',
+	directives: [ROUTER_DIRECTIVES]
+})
+export class ActivityComponent {
+
+}
