c92e6d5aaad769babe382b85b773a79daf74267f
Gautam Singh <gtm93singh@gmail.com>
2016-05-16 03:27:19 +0530

    bug fixes

--- api/init.php
+++ api/init.php
@@ -1,11 +1,20 @@
 <?php
 
-copy('./sample.config.example.json', './config.json');
-copy('./sample.city.json', './city.json');
-copy('./sample.event.json', './event.json');
-copy('./sample.event_user.json', './event_user.json');
-copy('./sample.interest.json', './interest.json');
-copy('./sample.interest_event.json', './interest_event.json');
-copy('./sample.session.json', './session.json');
-copy('./sample.user.json', './user.json');
-copy('./sample.user_event.json', './user_event.json');
+function copyFile($src, $dest, $append = true)
+{
+    $prefix = __DIR__;
+    if ( $append ) {
+        $prefix .= '/data/';
+    }
+    copy($prefix . $src, $prefix . $dest);
+}
+
+copyFile('config.example.json', 'config.json', false);
+copyFile('sample.city.json', 'city.json');
+copyFile('sample.event.json', 'event.json');
+copyFile('sample.event_user.json', 'event_user.json');
+copyFile('sample.interest.json', 'interest.json');
+copyFile('sample.interest_event.json', 'interest_event.json');
+copyFile('sample.session.json', 'session.json');
+copyFile('sample.user.json', 'user.json');
+copyFile('sample.user_event.json', 'user_event.json');
