3c12ef2772eb839e55769252967334c38ba70aff
Gautam Singh <gtm93singh@gmail.com>
2016-05-16 03:22:23 +0530

    Added init file and favicon

---
+++ api/init.php
@@ -0,0 +1,11 @@
+<?php
+
+copy('./config.example.json', './config.json');
+copy('./city.json', './city.json');
+copy('./event.json', './event.json');
+copy('./event_user.json', './event_user.json');
+copy('./interest.json', './interest.json');
+copy('./interest_event.json', './interest_event.json');
+copy('./session.json', './session.json');
+copy('./user.json', './user.json');
+copy('./user_event.json', './user_event.json');
