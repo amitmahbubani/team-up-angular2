04883b7db7bcb0e4e4e776084c924dadc3479195
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-15 13:20:26 +0530

    event join api call added

--- app/components/result/result.component.ts
+++ app/components/result/result.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {ROUTER_DIRECTIVES, Router} from '@angular/router-deprecated';
 import {EventService} from '../common/event.service';
 import {UserService} from '../common/user.service'
 
@@ -11,10 +11,28 @@ import {UserService} from '../common/user.service'
 export class ResultComponent {
 	public eventResultSet = [];
 	public currentCount = 0;
-	constructor(public userService: UserService, private eventService: EventService) {
+	constructor(public userService: UserService, private eventService: EventService, private router: Router) {
 		this.eventResultSet = this.eventService.eventResultList;
 	}
 	getNextResult() {
 		this.currentCount++;
+	}
+	joinEvent(event_id){
+		var body = {
+			event_id:event_id
+		}
+		this.eventService.joinEvent(body).subscribe(
+			data => {
+				if (data.success) {
+					alert("You have Joined this activity.");
+					this.router.navigate(['Home']);
+				} else {
+					//TBD
+				}
+			},
+			err => {
+				console.log('Error Occured', err);
+			}
+		);
 	}
 }
