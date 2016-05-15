df30d3aa005da7b54c05edf5f199e770a779919a
Gautam Singh <gtm93singh@gmail.com>
2016-05-16 05:05:43 +0530

    Removed logs and sample city_event model

--- api/init.php
+++ api/init.php
@@ -18,3 +18,4 @@ copyFile('sample.interest_event.json', 'interest_event.json');
 copyFile('sample.session.json', 'session.json');
 copyFile('sample.user.json', 'user.json');
 copyFile('sample.user_event.json', 'user_event.json');
+copyFile('sample.city_event.json', 'city_event.json');
